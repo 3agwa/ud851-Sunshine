994d093f6ab0887ce25dd91e8bfbce4ecba75c92
3agwa <a.mounir.112@gmail.com>
2019-12-30 15:12:21 +0200

    added a menu item in the detail activity that shares its contents

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -211,6 +211,25 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
         return true;
     }
 
+    private void openLocationMap()
+    {
+        String addressString = "1600 Ampitheatre Parkway, CA";
+        Uri geoLocation = Uri.parse("geo:0,0?q=" + addressString);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Log.d(TAG, "Couldn't call " + geoLocation.toString()
+                    + ", no receiving apps installed!");
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -221,8 +240,12 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
             return true;
         }
 
-        // TODO (2) Launch the map when the map menu item is clicked
-
+        // COMPLETED (2) Launch the map when the map menu item is clicked
+        if (id == R.id.action_refresh)
+        {
+            openLocationMap();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
